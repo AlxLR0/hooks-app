0551af77f60f598a45fe4d740770346b1dfe43ba
Alejandro <alex.lopez.rubio2600@gmail.com>
2026-09-25 15:30:52 -0600

    feat: update dependencies and add new components
    - Added new dependencies: @base-ui/react, @fontsource-variable/geist, class-variance-authority, cn, lucide-react, shadcn, and tw-animate-css.
    - Introduced FocusScreen component for input focus management.
    - Created TaskApp component for task management with useReducer.
    - Implemented reusable UI components: Button, Card, Checkbox, and Input.
    - Enhanced PokemonPage to fetch and display Pokémon data dynamically.
    - Updated index.css for new styles and themes.
    - Configured TypeScript paths for easier imports.
    - Updated Vite configuration to support path aliases.

--- src/03-examples/pokemonPage.tsx
+++ src/03-examples/pokemonPage.tsx
@@ -1,16 +1,34 @@
 import { useCounter } from "../hooks/useCounter";
+import { usePokemon } from "../hooks/usePokemon";
 
 export const PokemonPage = () => {
 
     const {counter,increment, decrement} = useCounter();
+    const {pokemon,isLoading} = usePokemon({id:counter});
+
+    if (isLoading) {
+      return(
+        <div className="bg-gradient flex flex-col items-center">
+          <h1 className="text-xl font-bold text-white animate-bounce">Cargando...</h1>
+        </div>
+      )
+    }
+
+    if (!pokemon) {
+      return(
+        <div className="bg-gradient flex flex-col items-center">
+          <h1 className="text-xl font-bold text-white animate-bounce">Pokemon no encontrado </h1>
+        </div>
+      )
+    }
 
   return (
     <div className="bg-gradient flex flex-col items-center">
       <h1 className="text-2xl font-thin text-white">Pokémon</h1>
-      <h3 className="text-xl font-bold text-white">#{counter} Bulbasaur</h3>
+      <h3 className="text-xl font-bold text-white">#{counter} {pokemon?.name}</h3>
       <img
-        src={}
-        alt=""
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${counter}.png`}
+        alt={pokemon.name}
       />
 
       <div className="flex gap-2">
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { PokemonPage } from './03-examples/pokemonPage'
+import { TasksApp } from './05-useReducer/TaskApp'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+// import { PokemonPage } from './03-examples/pokemonPage'
 // import { TrafficLightEffectHook } from './02-useEffect/TrafficLightEffectHook'
 // import { TrafficLightEffect } from './02-useEffect/TrafficLightEffect'
 // import { TrafficLight } from './01-useState/TrafficLight'
@@ -14,6 +16,8 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLight></TrafficLight> */}
     {/* <TrafficLightEffect></TrafficLightEffect> */}
     {/* <TrafficLightEffectHook></TrafficLightEffectHook> */}
-    <PokemonPage></PokemonPage>
+    {/* <PokemonPage></PokemonPage> */}
+    {/* <FocusScreen></FocusScreen> */}
+    <TasksApp></TasksApp>
   </StrictMode>,
 )
